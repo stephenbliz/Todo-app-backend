7b455f647c10153c0629141cc1a670d4248d7b48
bliz <uzomaebuka11@gmail.com>
2025-07-23 08:18:38 +0100

    Created a jwt middleware function to validation users for protected api routes

---
+++ middlewares/jwtValidation.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const jwtValidation = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token){
+        return res.status(404).json({message: 'No token'});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch(err){
+        return res.status(401).json({message: 'Not authorised', err});
+    }
+    
+}
+
+module.exports = jwtValidation;
