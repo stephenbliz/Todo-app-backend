955df9824a0361445362e84a5334c37319ccd302
bliz <uzomaebuka11@gmail.com>
2025-07-28 06:31:45 +0100

    Modified the jwtValidation middleware to include res.status 401 for invalid and token not found

--- middlewares/jwtValidation.js
+++ middlewares/jwtValidation.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const jwtValidation = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
     if(!token){
-        return res.status(404).json({message: 'No token'});
+        return res.status(401).json({message: 'No token'});
     }
 
     try{
